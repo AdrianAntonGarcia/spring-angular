f2e09cd2bf04192067c4320acd7467801704c250
Adrián Antón <adrtler@gmail.com>
2022-02-28 17:53:42 +0100

    Creando el componente clientes.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, ClientesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
